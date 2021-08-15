32a63f899cd51120591d6cdf49c52d3a47ff580f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-15 09:56:57 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+class Ball {
+    constructor(color = "regular") {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+        this.bounceLevel = bounceLevel;
+    }
+}
+
+const tennis = new Ball("yellow");
+console.log(tennis.color);
