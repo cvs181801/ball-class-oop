0a5783442f0eacbf74f6518326dc2125b813453a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-15 10:06:14 -0700

    now try it the ES5 way

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 
 
-class Ball {
-    constructor(color = "regular") {
-        this.height = height;
-        this.width = width;
-        this.color = color;
-        this.bounceLevel = bounceLevel;
-    }
+// class Ball {
+//     constructor(color = "regular") {
+//         // this.height = height;
+//         // this.width = width;
+//         this.color = color;
+//         //this.bounceLevel = bounceLevel;
+//     }
+// }
+
+// const tennis = new Ball("yellow");
+// console.log(tennis.color);
+
+// const regular = new Ball();
+// console.log(regular.color);
+
+// const football = new Ball("brown");
+// console.log(football.color);
+
+// const baseball = new Ball("white");
+// console.log(baseball.color);
+
+// now try the ES5 way ~~~
+
+function Ball(color) {
+    this.color = color;
 }
 
-const tennis = new Ball("yellow");
-console.log(tennis.color);
+const yellowBall = new Ball("yellow");
+console.log(yellowBall.color);
+
+const redBall = new Ball("red");
+console.log(redBall.color);
+
